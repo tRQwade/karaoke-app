fea649f9ab3f17eaf30f29ef1b3ab7e07ec25fe7
tRQwade <82306935+tRQwade@users.noreply.github.com>
2025-08-03 14:50:17 -0700

    Stylize request form for DiCicco's theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,15 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        cinzel: ["Cinzel", "serif"],
+      },
+      colors: {
+        diciccoRed: "#C00000",
+        diciccoGreen: "#1D7D38",
+      },
       animation: {
         "ping-once": "ping 0.8s ease-in-out 1",
-      },
-      extend: {
-        fontFamily: {
-          cinzel: ["Cinzel", "serif"],
-        },
-        colors: {
-          diciccoRed: "#C00000",
-          diciccoGreen: "#1D7D38",
-        },
       },
     },
   },
